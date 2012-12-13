1989dc1c222661fd26e33fe4b85134445924eba5
Nate Dickson <natedickson@ldschurch.org>
2012-12-13 13:31:28 -0700

    turned planter into a class instead of a regular script.

--- lib/planter.rb
+++ lib/planter.rb
@@ -16,86 +16,79 @@
 require 'yaml'
 require 'tmpdir'
 require 'fileutils'
-
-def get_hierarchy(input,parent=".",dirs_to_create=[])
-  input.each do |dirs|
-    if dirs.kind_of? Hash
-      dirs.each do |k,v|
+class Planter
+  def get_hierarchy(input, parent=".", dirs_to_create=[])
+    input.each do |dirs|
+      if dirs.kind_of? Hash
+        dirs.each do |k, v|
           dirs_to_create.push(File.expand_path("#{parent}/#{k.strip}"))
-          dirs_to_create = get_hierarchy(v,"#{parent}/#{k.strip}",dirs_to_create)
+          dirs_to_create = get_hierarchy(v, "#{parent}/#{k.strip}", dirs_to_create)
+        end
+      elsif dirs.kind_of? Array
+        dirs_to_create = get_hierarchy(dirs, parent, dirs_to_create)
+      elsif dirs.kind_of? String
+        dirs_to_create.push(File.expand_path("#{parent}/#{dirs.strip}"))
       end
-    elsif dirs.kind_of? Array
-      dirs_to_create = get_hierarchy(dirs,parent,dirs_to_create)
-    elsif dirs.kind_of? String
-      dirs_to_create.push(File.expand_path("#{parent}/#{dirs.strip}"))
     end
+    return dirs_to_create
   end
-  return dirs_to_create
-end
 
-def text_to_yaml(input, replacements = [])
-  lines = input.split("\n")
-  #output = []  #this doesn't seem to be used anywhere?
-  prev_indent = 0
-  lines.each_with_index do |line, i|
-    if line =~ /%%(\d+)%%/
-      if $1.to_i <= replacements.length
-        line.gsub!(/%%#{$1}%%/,replacements[$1.to_i - 1])
+  def text_to_yaml(input, replacements = [])
+    lines       = input.split("\n")
+    #output = []  #this doesn't seem to be used anywhere?
+    prev_indent = 0
+    lines.each_with_index do |line, i|
+      if line =~ /%%(\d+)%%/
+        if $1.to_i <= replacements.length
+          line.gsub!(/%%#{$1}%%/, replacements[$1.to_i - 1])
+        else
+          $stderr.puts('Mismatch in number of template variables found and replacements provided')
+          lines[i] = ''
+          next
+        end
+      end
+      indent = line.gsub(/  /, "\t").match(/(\t*).*$/)[1]
+      if indent.length > prev_indent
+        lines[i-1] = lines[i-1].chomp + ":"
+      end
+      prev_indent = indent.length
+      lines[i]    = indent.gsub(/\t/, '  ') + "- " + lines[i].strip # unless indent.length == 0
+    end
+    lines.delete_if { |line|
+      line == ''
+    }
+    return "---\n" + lines.join("\n")
+  end
+
+  def plant(incoming = nil)
+    data = ""
+    if incoming != nil
+      if File.exists? incoming
+        template = incoming        
       else
-        $stderr.puts('Mismatch in number of template variables found and replacements provided')
-        lines[i] = ''
-        next
+        STDERR.puts "Could not read provided template."
+        exit 1
       end
+    else
+      template = File.expand_path("~/.planter/#{incoming.gsub(/\.tpl$/, '')}.tpl")
+      ARGV.shift
     end
-    indent = line.gsub(/  /,"\t").match(/(\t*).*$/)[1]
-    if indent.length > prev_indent
-      lines[i-1] = lines[i-1].chomp + ":"
+    if File.exists? template
+      File.open(template, 'r') do |infile|
+        data = infile.read
+      end
+    else
+      STDERR.puts "Specified template not found in ~/.planter/*.tpl"
+      exit 1
     end
-    prev_indent = indent.length
-    lines[i] = indent.gsub(/\t/,'  ') + "- " + lines[i].strip # unless indent.length == 0
-  end
-  lines.delete_if {|line|
-    line == ''
-  }
-  return "---\n" + lines.join("\n")
-end
+    data.strip!
 
-if STDIN.stat.size > 0
-  data = STDIN.read
-elsif ARGV.length > 0
-  if File.exists? ARGV[0]
-    template = ARGV[0]    
-    ARGV.shift
-  else
-    template = File.expand_path("~/.planter/#{ARGV[0].gsub(/\.tpl$/,'')}.tpl")
-    ARGV.shift
-  end
-  if File.exists? template
-    File.open(template, 'r') do |infile|
-      data = infile.read
+    yaml           = YAML.load(text_to_yaml(data, ARGV))
+    dirs_to_create = get_hierarchy(yaml)
+
+    dirs_to_create.each do |dir|
+      $stderr.puts "Creating #{dir}"
+      Dir.mkdir(dir) unless File.exists? dir
     end
-  else
-    STDERR.puts "Specified template not found in ~/.planter/*.tpl"
-  end
-else
-  tmpfile = File.expand_path(Dir.tmpdir + "/planter.tmp")
-  File.new(tmpfile, 'a+')
-
-  # at_exit {FileUtils.rm(tmpfile) if File.exists?(tmpfile)}
-
-  %x{$EDITOR "#{tmpfile}"}
-  data = ""
-  File.open(tmpfile, 'r') do |infile|
-    data = infile.read
-  end
-end
-
-data.strip!
-
-yaml = YAML.load(text_to_yaml(data,ARGV))
-dirs_to_create = get_hierarchy(yaml)
-
-dirs_to_create.each do |dir|
-  $stderr.puts "Creating #{dir}"
-  Dir.mkdir(dir) unless File.exists? dir
-end
+  end #def plant
+end #class Planter
