449e4ac44baff4882e70371f4e9a28d69b17d0ac
Nate Dickson <natedickson@ldschurch.org>
2013-01-17 14:54:37 -0700

    Just linking in libraries, don't get excited.

--- lib/markedblog.rb
+++ lib/markedblog.rb
@@ -1,4 +1,6 @@
 require 'markedblog/version.rb'
+require 'planter.rb'
+require 'settings.rb'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
